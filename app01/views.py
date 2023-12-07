01c5056220dc41fe4203483a4cdf3c5ed7c20f06
mst-mkt <s1f102303057@iniad.org>
2023-12-07 16:51:07 +0900

    add: some pages in app01

--- app01/views.py
+++ app01/views.py
@@ -3,3 +3,14 @@ from django.http import HttpRequest, HttpResponse
 
 def root(_: HttpRequest) -> HttpResponse:
     return HttpResponse("Hello Django")
+
+
+def pattern(_: HttpRequest, username: str) -> HttpResponse:
+    return HttpResponse(f"Hello {username}")
+
+
+def param(request: HttpRequest) -> HttpResponse:
+    text = ""
+    for key in request.GET:
+        text += f"{key} : {request.GET[key]}, "
+    return HttpResponse(text)
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,7 @@ import app01.views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("top/", app01.views.root),
+    path("", app01.views.root, name="root"),
+    path("app01/pattern/<username>/", app01.views.pattern, name="pattern"),
+    path("app01/param/", app01.views.param, name="param"),
 ]
