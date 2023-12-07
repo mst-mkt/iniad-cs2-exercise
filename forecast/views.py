9a7909d3c0e010f6b3d808b131656d9a3bab0ed0
mst-mkt <s1f102303057@iniad.org>
2023-12-07 22:54:19 +0900

    feat: dispaly one week forecast(random)

--- forecast/views.py
+++ forecast/views.py
@@ -7,7 +7,7 @@ weathers = ["Sunny", "Rainy", "Cloudy"]
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    params: dict[str, str] = {}
+    params: dict[str, str | list[str]] = {}
     params["title"] = "3 days forecast"
-    params["forecasts"] = random.choice(weathers)
+    params["forecasts"] = random.choices(weathers, k=7)
     return render(request, "forecast/index.html", params)
