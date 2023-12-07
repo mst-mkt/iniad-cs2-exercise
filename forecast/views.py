44176a01d50f3ac6de173beee5c0fae5f85f259d
mst-mkt <s1f102303057@iniad.org>
2023-12-07 17:16:00 +0900

    feat: random forecast page

--- forecast/views.py
+++ forecast/views.py
@@ -1,8 +1,13 @@
+import random
+
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+weathers = ["Sunny", "Rainy", "Cloudy"]
+
 
 def index(request: HttpRequest) -> HttpResponse:
-    params = {}
+    params: dict[str, str] = {}
     params["title"] = "3 days forecast"
+    params["forecasts"] = random.choice(weathers)
     return render(request, "forecast/index.html", params)
